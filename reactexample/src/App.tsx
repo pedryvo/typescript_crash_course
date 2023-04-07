d84c3eefd7ca5e513cc6d0ad7b137f6db06a870c
Pedro Ivo <pedryvo@gmail.com>
2023-04-07 17:47:51 -0300

    full course

---
+++ reactexample/src/App.tsx
@@ -0,0 +1,12 @@
+import Header from './Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header title='Test'/>
+    </div>
+  );
+}
+
+export default App;
